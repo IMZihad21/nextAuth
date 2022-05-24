b3d513f813ec0f42276d8a3f26778fef2f488bf6
Mofajjal Rasul <mofajjal.techforing@gmail.com>
2022-05-24 18:39:05 +0600

    Added react hook form with validation testing
    Signed-off-by: Mofajjal Rasul <mofajjal.techforing@gmail.com>

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,17 +1,17 @@
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function middleware(req: NextRequest, res: NextResponse) {
-  const url = req.nextUrl.clone();
+export async function middleware(req: NextRequest) {
   // return early if url isn't supposed to be protected
-  if (url.pathname.includes("/auth")) {
+  if (!!req.url.match(/\/auth/) || !req.page.name) {
     return NextResponse.next();
   }
 
   const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  console.log(req.url);
   // You could also check for any property on the session object,
   // like role === "admin" or name === "John Doe", etc.
-  if (!session) return NextResponse.rewrite(new URL("/auth", req.url));
+  if (!session) return NextResponse.redirect(new URL("/auth", req.url));
 
   // If user is authenticated, continue.
   return NextResponse.next();
--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -1,7 +1,8 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import type { GetServerSideProps, NextPage } from "next";
-import { getCsrfToken, getSession } from "next-auth/react";
+import { getCsrfToken, getSession, signIn } from "next-auth/react";
 import * as React from "react";
+import { useForm } from "react-hook-form";
 
 // Export the `session` prop to use sessions with Server Side Rendering
 export const getServerSideProps: GetServerSideProps = async (context) => {
@@ -20,23 +21,71 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 };
 
 const Authentication: NextPage = ({ csrfToken }: any) => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data: any) => {
+    const { email, password } = data;
+    signIn("credentials", { email, password });
+  };
+
   return (
     <Box>
       <Typography variant="h4" component="h1" gutterBottom textAlign="center">
         Next.js with TypeScript and Material-UI
       </Typography>
-      <form method="post" action="/api/auth/callback/credentials">
-        <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-        <label>
-          Email
-          <input name="email" type="text" />
-        </label>
-        <label>
-          Password
-          <input name="password" type="password" />
-        </label>
-        <button type="submit">Sign in</button>
-      </form>
+      <Box
+        component="form"
+        onSubmit={handleSubmit(onSubmit)}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "1rem",
+          px: "20rem",
+        }}
+      >
+        <TextField
+          label="Email"
+          variant="outlined"
+          error={Boolean(errors.email)}
+          helperText={errors.email && errors.email.message}
+          {...register("email", {
+            required: true,
+            pattern: {
+              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+              message: "Invalid Email Address",
+            },
+          })}
+        />
+        <TextField
+          label="Password"
+          variant="outlined"
+          type="password"
+          error={Boolean(errors.password)}
+          helperText={errors.password && errors.password.message}
+          {...register("password", {
+            required: true,
+            pattern: {
+              value:
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+              message:
+                "Password must contain at least one uppercase, one lowercase, one number and one special character",
+            },
+          })}
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          sx={{
+            py: "10px",
+          }}
+        >
+          Submit
+        </Button>
+      </Box>
     </Box>
   );
 };
