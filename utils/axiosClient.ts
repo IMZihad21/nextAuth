b4f1aec1d90ad422cbf7c24f9a08a13a5ce3f2b8
ZèD <imzihad@gmail.com>
2022-06-12 08:32:17 +0600

    Switched to cloudflare server

--- utils/axiosClient.ts
+++ utils/axiosClient.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosClient = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://graphql-svr.herokuapp.com/"
+      ? "https://cgql.imzihad21.workers.dev/"
       : "http://localhost:4000/",
   withCredentials: true,
 });
